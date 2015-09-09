a1cf1c2548300b32c66148eae56f3b3485a2977d
Goodlike <henrisssss@hotmail.com>
2015-09-09 18:13:16 +0300

    Added new constant

--- src/main/java/eu/goodlike/misc/Constants.java
+++ src/main/java/eu/goodlike/misc/Constants.java
@@ -19,6 +19,17 @@ public final class Constants {
      */
     public static final String MYSQL_GROUP_CONCAT_SEPARATOR = ",";
 
+    /**
+     * <pre>
+     * Default value I choose to use for VARCHAR table fields when there is no good reason to reduce/increase it
+     *
+     * This value arises from utf8mb4 char size and primary key limit, which becomes exceeded at around 190 chars;
+     * Since I sometimes had to use two VARCHAR columns, 90 felt better than 95, thus 180 seems better than 190 where
+     * I only need 1 VARCHAR field
+     * </pre>
+     */
+    public static final int DEFAULT_VARCHAR_FIELD_SIZE = 180;
+
     // PRIVATE
 
     private Constants() {
