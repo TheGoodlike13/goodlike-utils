52027bca484141e13297cb65e4418888b37a545c
Goodlike <henrisssss@hotmail.com>
2015-09-14 11:59:21 +0300

    Added minor optimizations to Null
    Now, when a single object is being checked, it is checked first, and a specialized Null instance is returned which will either immediately throw the exception, or just simply do nothing, rather than constructing arrays/lists etc

--- src/main/java/eu/goodlike/neat/Null.java
+++ src/main/java/eu/goodlike/neat/Null.java
@@ -1,8 +1,8 @@
 package eu.goodlike.neat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -26,7 +26,7 @@ import java.util.function.Supplier;
  * including your message, allowing to easily figure out the location of null(s)
  * </pre>
  */
-public final class Null {
+public class Null {
 
     /**
      * <pre>
@@ -45,39 +45,42 @@ public final class Null {
      */
     public void ifAny(String message) {
         int index = objects.indexOf(null);
-        if (index >= 0) {
-            if (objects.size() > 1)
-                message = message + "; parameter at index " + index + " was null, please check: " + objects;
-
-            throw new NullPointerException(message);
-        }
+        if (index >= 0)
+            throw new NullPointerException(message + "; parameter at index "
+                    + index +" was null, please check: " + objects);
     }
 
     // CONSTRUCTORS
 
     public static Null check(Object... objects) {
-        return new Null(Arrays.asList(objects));
+        return objects == null ? DEFINITELY_NULL : new Null(Arrays.asList(objects));
+    }
+
+    public static Null check(Object singleObject) {
+        return singleObject == null ? DEFINITELY_NULL : DEFINITELY_NOT_NULL;
     }
 
     /**
      * Use this method when you need to check something like a primitive array for null by itself
+     * to avoid ambiguous call warnings
      */
     public static Null checkAlone(Object possiblyArray) {
-        return new Null(Collections.singletonList(possiblyArray));
+        return possiblyArray == null ? DEFINITELY_NULL : DEFINITELY_NOT_NULL;
     }
 
     /**
      * Use this method when you need to check all array elements for null (including array itself)
+     * to avoid ambiguous call warnings
      */
     public static Null checkArray(Object[] definitelyArray) {
-        return definitelyArray == null ? checkAlone(null) : check(definitelyArray);
+        return definitelyArray == null ? DEFINITELY_NULL : check(definitelyArray);
     }
 
     /**
      * Use this method when you need to check all collection elements for null (including collection itself)
      */
     public static Null checkCollection(Collection<?> collection) {
-        return collection == null ? checkAlone(null) : check(collection);
+        return collection == null ? DEFINITELY_NULL : new Null(new ArrayList<>(collection));
     }
 
     private Null(List<Object> objects) {
@@ -87,5 +90,72 @@ public final class Null {
     // PRIVATE
 
     private final List<Object> objects;
+
+    private static final Null DEFINITELY_NULL = new DefinitelyNull();
+    private static final Null DEFINITELY_NOT_NULL = new DefinitelyNotNull();
+
+    // SUBCLASSES
+
+    /**
+     * <pre>
+     * This is used when:
+     *      a) a single object was checked for null
+     *      b) this object was indeed null
+     *
+     * It avoids Null object construction and varargs array creation entirely
+     * </pre>
+     */
+    public static final class DefinitelyNull extends Null {
+        /**
+         * <pre>
+         * Allows customizing the kind of exception that is thrown using lambda expression:
+         *      () -> new X()
+         * </pre>
+         * @throws X because this object is definitely null
+         */
+        public <X extends Throwable> void ifAny(Supplier<? extends X> exceptionSupplier) throws X {
+            throw exceptionSupplier.get();
+        }
+
+        /**
+         * @throws NullPointerException because this object is definitely null
+         */
+        public void ifAny(String message) {
+            throw new NullPointerException(message);
+        }
+
+        private DefinitelyNull() {
+            super(null);
+        }
+    }
+
+    /**
+     * <pre>
+     * This is used when:
+     *      a) a single object was checked for null
+     *      b) this object was actually not null
+     *
+     * It avoids Null object construction and varargs array creation entirely
+     * </pre>
+     */
+    public static final class DefinitelyNotNull extends Null {
+        /**
+         * <pre>
+         * Allows customizing the kind of exception that is thrown using lambda expression:
+         *      () -> new X()
+         * </pre>
+         * @throws X never, because this object is definitely not null
+         */
+        public <X extends Throwable> void ifAny(Supplier<? extends X> exceptionSupplier) throws X {}
+
+        /**
+         * @throws NullPointerException never because this object is definitely null
+         */
+        public void ifAny(String message) {}
+
+        private DefinitelyNotNull() {
+            super(null);
+        }
+    }
 
 }
