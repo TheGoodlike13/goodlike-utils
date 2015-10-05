a445ae6e591558b5350ac3413565a6e7e9d3e407
Goodlike <henrisssss@hotmail.com>
2015-10-05 12:03:59 +0300

    Added a neat way to construct Strings

---
+++ src/main/java/eu/goodlike/neat/string/Str.java
@@ -0,0 +1,47 @@
+package eu.goodlike.neat.string;
+
+import eu.goodlike.neat.Null;
+
+/**
+ * <pre>
+ * Creates StringBuilderWrapper instances
+ *
+ * StringBuilderWrapper allows simple but neat way to construct a String using chained methods
+ *
+ * In general, if you feel that you need to explicitly define a variable of StringBuilderWrapper, you might as well
+ * just use StringBuilder in the first place; the wrapper is intended to take care of simple cases only
+ * </pre>
+ */
+public final class Str {
+
+    /**
+     * @return empty StringBuilderWrapper
+     */
+    public static StringBuilderWrapper of() {
+        return new StringBuilderWrapper();
+    }
+
+    /**
+     * @return StringBuilderWrapper containing all the objects, appended together
+     * @throws NullPointerException if object array is null (NOT if it contains null, that is allowed)
+     */
+    public static StringBuilderWrapper of(Object... objects) {
+        return new StringBuilderWrapper().and(objects);
+    }
+
+    /**
+     * @return StringBuilderWrapper containing all the objects, appended together, using the provided StringBuilder
+     * @throws NullPointerException if customBuilder or object array is null (NOT if it contains null, that is allowed)
+     */
+    public static StringBuilderWrapper of(StringBuilder customBuilder, Object... objects) {
+        Null.check(customBuilder).ifAny("Custom builder cannot be null");
+        return new StringBuilderWrapper(customBuilder).and(objects);
+    }
+
+    // PRIVATE
+
+    private Str() {
+        throw new AssertionError("Do not instantiate, use static methods!");
+    }
+
+}
---
+++ src/main/java/eu/goodlike/neat/string/StringBuilderWrapper.java
@@ -0,0 +1,186 @@
+package eu.goodlike.neat.string;
+
+import eu.goodlike.neat.Null;
+
+import java.util.Collection;
+import java.util.function.BiConsumer;
+
+/**
+ * <pre>
+ * Wraps StringBuilder in a convenient way
+ *
+ * Use Str.of() to create instances
+ *
+ * In general, if you feel that you need to explicitly define a variable of StringBuilderWrapper, you might as well
+ * just use StringBuilder in the first place; the wrapper is intended to take care of simple cases only
+ * </pre>
+ */
+public final class StringBuilderWrapper {
+
+    /**
+     * Appends all the objects, one after another, to the wrapped StringBuilder
+     * @throws NullPointerException if object array is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper and(Object... objects) {
+        Null.checkAlone(objects).ifAny("Object array cannot be null");
+        for (Object o : objects)
+            builder.append(o);
+        return this;
+    }
+
+    /**
+     * Appends all the objects in the collection, one after another, to the wrapped StringBuilder
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andSome(Collection<?> collection) {
+        Null.check(collection).ifAny("Collection cannot be null");
+        collection.forEach(builder::append);
+        return this;
+    }
+
+    /**
+     * <pre>
+     * Appends all the objects in the collection to the wrapped StringBuilder
+     * Every object will have a prefix appended before it (can be null)
+     * </pre>
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andSome(Object prefix, Collection<?> collection) {
+        Null.check(collection).ifAny("Collection cannot be null");
+        collection.forEach(o -> builder.append(prefix).append(o));
+        return this;
+    }
+
+    /**
+     * <pre>
+     * Appends all the objects in the collection to the wrapped StringBuilder
+     * Every object will have a suffix appended after it (can be null)
+     * </pre>
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andSome(Collection<?> collection, Object suffix) {
+        Null.check(collection).ifAny("Collection cannot be null");
+        collection.forEach(o -> builder.append(o).append(suffix));
+        return this;
+    }
+
+    /**
+     * <pre>
+     * Appends all the objects in the collection to the wrapped StringBuilder
+     * Every object will have a prefix and suffix appended before and after it (can be null)
+     * </pre>
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andSome(Object prefix, Collection<?> collection, Object suffix) {
+        Null.check(collection).ifAny("Collection cannot be null");
+        collection.forEach(o -> builder.append(prefix).append(o).append(suffix));
+        return this;
+    }
+
+    /**
+     * <pre>
+     * Appends all the objects in the collection to the wrapped StringBuilder
+     * Uses a custom consumer to specify how to append the elements of the collection
+     * </pre>
+     * @throws NullPointerException if customAppender or collection is null (NOT if it contains null, that is allowed)
+     */
+    public <T> StringBuilderWrapper andSome(Collection<T> collection, BiConsumer<StringBuilder, T> customAppender) {
+        Null.check(collection, customAppender).ifAny("Collection and customAppender cannot be null");
+        for (T t : collection)
+            customAppender.accept(builder, t);
+        return this;
+    }
+
+    /**
+     * Appends all the objects, one after another, to the wrapped StringBuilder, but only if condition is true
+     * @throws NullPointerException if object array is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andIf(boolean condition, Object... objects) {
+        return condition ? and(objects) : this;
+    }
+
+    /**
+     * Appends all the objects in the collection, one after another, to the wrapped StringBuilder,
+     * but only if condition is true
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andSomeIf(boolean condition, Collection<?> collection) {
+        return condition ? andSome(collection) : this;
+    }
+
+    /**
+     * <pre>
+     * Appends all the objects in the collection to the wrapped StringBuilder, but only if condition is true
+     * Every object will have a prefix appended before it (can be null)
+     * </pre>
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andSomeIf(boolean condition, Object prefix, Collection<?> collection) {
+        return condition ? andSome(prefix, collection) : this;
+    }
+
+    /**
+     * <pre>
+     * Appends all the objects in the collection to the wrapped StringBuilder, but only if condition is true
+     * Every object will have a suffix appended after it (can be null)
+     * </pre>
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andSomeIf(boolean condition, Collection<?> collection, Object suffix) {
+        return condition ? andSome(collection, suffix) : this;
+    }
+
+    /**
+     * <pre>
+     * Appends all the objects in the collection to the wrapped StringBuilder, but only if condition is true
+     * Every object will have a prefix and suffix appended before and after it (can be null)
+     * </pre>
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public StringBuilderWrapper andSomeIf(boolean condition, Object prefix, Collection<?> collection, Object suffix) {
+        return condition ? andSome(prefix, collection, suffix) : this;
+    }
+
+    /**
+     * <pre>
+     * Appends all the objects in the collection to the wrapped StringBuilder, but only if condition is true
+     * Every object will have a prefix and suffix appended before and after it (can be null)
+     * </pre>
+     * @throws NullPointerException if collection is null (NOT if it contains null, that is allowed)
+     */
+    public <T> StringBuilderWrapper andSomeIf(boolean condition, Collection<T> collection, BiConsumer<StringBuilder, T> customAppender) {
+        return condition ? andSome(collection, customAppender) : this;
+    }
+
+    /**
+     * This is a terminal operation, in case you want to append additional objects in a custom way
+     * @return the wrapped builder
+     */
+    public StringBuilder toBuilder() {
+        return builder;
+    }
+
+    /**
+     * This is a terminal operation, in case you are done appending
+     * @return string of the wrapped builder
+     */
+    @Override
+    public String toString() {
+        return builder.toString();
+    }
+
+    // CONSTRUCTORS
+
+    StringBuilderWrapper() {
+        this(new StringBuilder());
+    }
+
+    StringBuilderWrapper(StringBuilder builder) {
+        this.builder = builder;
+    }
+
+    // PRIVATE
+
+    private final StringBuilder builder;
+
+}
