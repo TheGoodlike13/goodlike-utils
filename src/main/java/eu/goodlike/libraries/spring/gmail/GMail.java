c3327fd3c0e27c820165a022ee51b1bf80ef1e37
Goodlike <henrisssss@hotmail.com>
2015-10-09 13:50:31 +0300

    Set up the GMail sender for use with Spring

--- src/main/java/eu/goodlike/libraries/spring/gmail/GMail.java
+++ src/main/java/eu/goodlike/libraries/spring/gmail/GMail.java
@@ -1,10 +1,17 @@
 package eu.goodlike.libraries.spring.gmail;
 
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
 /**
  * <pre>
  * Sends an email with GMail
  *
  * Intended for use with Spring
+ *
+ * Use getDefaultSender() in the @Bean method for convenience
  * </pre>
  */
 public interface GMail {
@@ -13,5 +20,18 @@ public interface GMail {
      * Send an email to given email, using title as subject and text as email body
      */
     void send(String emailTo, String title, String text);
+
+    static MailSender getDefaultSender(String username, String password) {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost("smtp.gmail.com");
+        sender.setPort(587);
+        sender.setUsername(username);
+        sender.setPassword(password);
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.setProperty("mail.smtp.auth", "true");
+        javaMailProperties.setProperty("mail.smtp.starttls.enable", "true");
+        sender.setJavaMailProperties(javaMailProperties);
+        return sender;
+    }
 
 }
--- src/main/java/eu/goodlike/libraries/spring/gmail/GMailSender.java
+++ src/main/java/eu/goodlike/libraries/spring/gmail/GMailSender.java
@@ -3,6 +3,17 @@ package eu.goodlike.libraries.spring.gmail;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
+/**
+ * <pre>
+ * How to set this up in Spring:
+ *      1) GMail config details can be found here: https://support.google.com/mail/answer/13287?hl=en (general)
+ *      2) Create a @Bean of MailSender
+ *          a) import username and password through Environment
+ *          b) use GMail.getDefaultSender(username, password)
+ *      3) Create a @Bean of GMail
+ *      4) Use GMail where you need it
+ * </pre>
+ */
 public final class GMailSender implements GMail {
 
     @Override
