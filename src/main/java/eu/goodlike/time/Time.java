72026584863b8f2ba64a8cf9721d927c60b56384
Goodlike <henrisssss@hotmail.com>
2015-11-24 01:39:32 +0200

    Fixed docs, and now uses new Caches utility

--- src/main/java/eu/goodlike/time/Time.java
+++ src/main/java/eu/goodlike/time/Time.java
@@ -1,7 +1,7 @@
 package eu.goodlike.time;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
+import eu.goodlike.cache.CacheWrapper;
+import eu.goodlike.cache.Caches;
 import eu.goodlike.neat.Null;
 import eu.goodlike.time.impl.TimeHandler;
 
@@ -10,12 +10,10 @@ import java.time.ZoneId;
 
 /**
  * <pre>
- * java.time.* version of joda time conversion; requires no additional libraries besides Guava
+ * Converts between time representations using a ZoneId where appropriate
  *
  * It seems that with the release of Java 8, joda time will no longer be updated a lot, thus I've decided to
  * move to the new standard as well
- *
- * The methods should be very similar to the ones in eu.goodlike.libraries.joda.time.* classes
  * </pre>
  */
 public final class Time {
@@ -60,7 +58,7 @@ public final class Time {
     }
 
     /**
-     * @return Date to Date converter for a joda LocalDate
+     * @return Date to Date converter for a java LocalDate
      */
     public static DateConverter convert(LocalDate date) {
         return atUTC().from(date);
@@ -76,9 +74,7 @@ public final class Time {
     // PRIVATE
 
     private static final ZoneId DEFAULT_TIME_ZONE = ZoneId.of("UTC");
-    private static final LoadingCache<ZoneId, TimeHandler> HANDLER_CACHE = Caffeine.newBuilder()
-            .softValues()
-            .build(TimeHandler::new);
+    private static final CacheWrapper<ZoneId, TimeHandler> HANDLER_CACHE = Caches.softCache(TimeHandler::new);
 
     private Time() {
         throw new AssertionError("Do not instantiate, use static methods!");
