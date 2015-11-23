04326592681d67e2a1616f8ba37cbdcd9221ffbd
Goodlike <henrisssss@hotmail.com>
2015-11-24 01:30:41 +0200

    Added documentation

--- src/main/java/eu/goodlike/libraries/DependencyCheck.java
+++ src/main/java/eu/goodlike/libraries/DependencyCheck.java
@@ -1,16 +1,33 @@
 package eu.goodlike.libraries;
 
+import eu.goodlike.neat.Null;
+
+/**
+ * Allows basic checking if certain dependencies are available; no guarantee as to version is made!
+ */
 public final class DependencyCheck {
 
+    /**
+     * @return true if Caffeine classes are available, false otherwise
+     */
     public static boolean isCaffeineAvailable() {
         return isAvailable("com.github.benmanes.caffeine.cache.LoadingCache");
     }
 
+    /**
+     * @return true if Guava classes are available, false otherwise
+     */
     public static boolean isGuavaAvailable() {
         return isAvailable("com.google.common.cache.LoadingCache");
     }
 
+    /**
+     * fullClassName should include the package, i.e. for this class: 'eu.goodlike.libraries.DependencyCheck'
+     * @return true if given class is available, false otherwise
+     * @throws NullPointerException if fullClassName is null
+     */
     public static boolean isAvailable(String fullClassName) {
+        Null.check(fullClassName).ifAny("Class name cannot be null");
         try {
             return Class.forName(fullClassName) != null;
         } catch (ClassNotFoundException e) {
