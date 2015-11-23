f73caf61cc11c5383cb82ba8760346f32d75f1c0
Goodlike <henrisssss@hotmail.com>
2015-11-24 00:56:44 +0200

    Added Futures utilities

---
+++ src/main/java/eu/goodlike/functional/Futures.java
@@ -0,0 +1,37 @@
+package eu.goodlike.functional;
+
+import eu.goodlike.misc.ThreadPools;
+import eu.goodlike.neat.Null;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Utility methods to work with CompletableFutures
+ */
+public final class Futures {
+
+    /**
+     * <pre>
+     * Suppose that you need to execute some kind of computation in a particular Thread, but the context of this Thread
+     * would only accept Runnables, and you required it to return a value - this method does exactly that
+     *
+     * Inspired by Android's runOnUiThread(Runnable)
+     * </pre>
+     * @return value of a supplier, executed in given context, wrapped in a CompletableFuture
+     */
+    public static <T> CompletableFuture<T> valueFromSupplierAsRunnable(Consumer<Runnable> context, Supplier<T> supplier) {
+        Null.check(context, supplier).ifAny("Context and supplier cannot be null");
+        BlockingSupplier<T> blockingSupplier = new BlockingSupplier<>();
+        context.accept(() -> blockingSupplier.set(supplier.get()));
+        return CompletableFuture.supplyAsync(blockingSupplier, ThreadPools.forShortTasks());
+    }
+
+    // PRIVATE
+
+    private Futures() {
+        throw new AssertionError("Do not instantiate, use static methods!");
+    }
+
+}
