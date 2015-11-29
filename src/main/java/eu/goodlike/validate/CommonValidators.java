d6d716c0f8a3892e34dc3129e7a7125dd6a28667
Goodlike <henrisssss@hotmail.com>
2015-11-29 20:11:15 +0200

    Updated CommonValidators

--- src/main/java/eu/goodlike/validate/CommonValidators.java
+++ src/main/java/eu/goodlike/validate/CommonValidators.java
@@ -1,50 +1,81 @@
 package eu.goodlike.validate;
 
-import eu.goodlike.tbr.validate.impl.IntegerValidator;
-import eu.goodlike.tbr.validate.impl.LongIntegerValidator;
-import eu.goodlike.tbr.validate.impl.StringValidator;
+import eu.goodlike.validate.impl.IntObjValidator;
+import eu.goodlike.validate.impl.LongObjValidator;
+import eu.goodlike.validate.impl.StringValidator;
 
 import static eu.goodlike.misc.Constants.DEFAULT_VARCHAR_FIELD_SIZE;
-import static eu.goodlike.tbr.validate.Validate.*;
+import static eu.goodlike.validate.Validate.Collections.codePoints;
+import static eu.goodlike.validate.Validate.*;
 
 /**
- * Hosts common validators that appear in my work (TO BE UPDATED!)
+ * Common validators that appear in my work
  */
 public final class CommonValidators {
 
     /**
-     * Validator to check if a string is null or blank
+     * Checks if String is not null and not blank
      */
-    public static final StringValidator NOT_NULL_NOT_BLANK = string().not().isNull().not().isBlank();
+    public static final StringValidator NOT_NULL_NOT_BLANK = str().not().isNull().not().isBlank();
 
     /**
-     * Validator to check if a string is null or blank
+     * Checks if String is null, otherwise not blank
      */
-    public static final StringValidator NULL_OR_NOT_BLANK = string().isNull().or().not().isBlank();
+    public static final StringValidator NULL_OR_NOT_BLANK = str().isNull().or().not().isBlank();
 
+    /**
+     * Checks if String is null, otherwise not blank and fits into DEFAULT_VARCHAR_FIELD_SIZE
+     */
     public static final StringValidator OPTIONAL_JSON_STRING =
-            string().isNull().or().not().isBlank().isNoLargerThan(DEFAULT_VARCHAR_FIELD_SIZE);
+            str().isNull().or().not().isBlank().passesAsCodePoints(codePoints().isAtMostSizeOf(DEFAULT_VARCHAR_FIELD_SIZE));
 
+    /**
+     * Checks if String is null, otherwise fits into DEFAULT_VARCHAR_FIELD_SIZE
+     */
     public static final StringValidator OPTIONAL_JSON_STRING_BLANK_ABLE =
-            string().isNull().or().isNoLargerThan(DEFAULT_VARCHAR_FIELD_SIZE);
+            str().isNull().or().passesAsCodePoints(codePoints().isAtMostSizeOf(DEFAULT_VARCHAR_FIELD_SIZE));
 
+    /**
+     * Checks if String is not null, not blank and fits into DEFAULT_VARCHAR_FIELD_SIZE
+     */
     public static final StringValidator MANDATORY_JSON_STRING = NOT_NULL_NOT_BLANK
-            .isNoLargerThan(DEFAULT_VARCHAR_FIELD_SIZE);
+            .passesAsCodePoints(codePoints().isAtMostSizeOf(DEFAULT_VARCHAR_FIELD_SIZE));
 
+    /**
+     * Checks if String is not null and fits into DEFAULT_VARCHAR_FIELD_SIZE
+     */
     public static final StringValidator MANDATORY_JSON_STRING_BLANK_ABLE =
-            string().not().isNull().isNoLargerThan(DEFAULT_VARCHAR_FIELD_SIZE);
+            str().not().isNull().passesAsCodePoints(codePoints().isAtMostSizeOf(DEFAULT_VARCHAR_FIELD_SIZE));
 
-    public static final IntegerValidator OPTIONAL_JSON_ID = integer().isNull().or().isAtLeast(1);
+    /**
+     * Checks if Integer is null, otherwise 1 or more
+     */
+    public static final IntObjValidator OPTIONAL_JSON_ID = oInt().isNull().or().isAtLeast(1);
 
-    public static final IntegerValidator MANDATORY_JSON_ID = integer().not().isNull().isAtLeast(1);
+    /**
+     * Checks if Integer is not null and 1 or more
+     */
+    public static final IntObjValidator MANDATORY_JSON_ID = oInt().not().isNull().isAtLeast(1);
 
-    public static final LongIntegerValidator OPTIONAL_JSON_LONG_ID = longInt().isNull().or().isAtLeast(1);
+    /**
+     * Checks if Long is null, otherwise 1 or more
+     */
+    public static final LongObjValidator OPTIONAL_JSON_LONG_ID = oLong().isNull().or().isAtLeast(1L);
 
-    public static final LongIntegerValidator MANDATORY_JSON_LONG_ID = longInt().not().isNull().isAtLeast(1);
+    /**
+     * Checks if Long is not null and 1 or more
+     */
+    public static final LongObjValidator MANDATORY_JSON_LONG_ID = oLong().not().isNull().isAtLeast(1L);
 
-    public static final LongIntegerValidator OPTIONAL_TIMESTAMP = longInt().isNull().or().isAtLeast(0);
+    /**
+     * Checks if Long is null, otherwise a valid timestamp (>= 0)
+     */
+    public static final LongObjValidator OPTIONAL_TIMESTAMP = oLong().isNull().or().isAtLeast(0L);
 
-    public static final LongIntegerValidator MANDATORY_TIMESTAMP = longInt().not().isNull().isAtLeast(0);
+    /**
+     * Checks if Long is not null and a valid timestamp (>= 0)
+     */
+    public static final LongObjValidator MANDATORY_TIMESTAMP = oLong().not().isNull().isAtLeast(0L);
 
     // PRIVATE
 
