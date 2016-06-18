2f5f7691c3ea6b2c87cb63549b4bdd829ee84422
Goodlike <henrisssss@hotmail.com>
2016-06-18 22:31:35 +0300

    Added String formatting methods
    These methods use the slf4j way of replacing objects, that is, replacing {} with given objects

--- src/main/java/eu/goodlike/neat/Str.java
+++ src/main/java/eu/goodlike/neat/Str.java
@@ -1,6 +1,8 @@
 package eu.goodlike.neat;
 
 import eu.goodlike.neat.impl.str.StringBuilderWrapper;
+import org.slf4j.helpers.FormattingTuple;
+import org.slf4j.helpers.MessageFormatter;
 
 /**
  * <pre>
@@ -38,10 +40,39 @@ public final class Str {
         return new StringBuilderWrapper(customBuilder).and(objects);
     }
 
+    /**
+     * @return String, formatted using the slf4j method (replace {} with given String values)
+     */
+    public static String format(String string, Object any) {
+        return format(MessageFormatter.format(string, any));
+    }
+
+    /**
+     * @return String, formatted using slf4j method (replace {}  with given String values)
+     */
+    public static String format(String string, Object any1, Object any2) {
+        return format(MessageFormatter.format(string, any1, any2));
+    }
+
+    /**
+     * @return String, formatted using slf4j method (replace {}  with given String values)
+     */
+    public static String format(String string, Object... any) {
+        return format(MessageFormatter.arrayFormat(string, any));
+    }
+
     // PRIVATE
 
     private Str() {
         throw new AssertionError("Do not instantiate, use static methods!");
+    }
+
+    private static String format(FormattingTuple formattingTuple) {
+        String result = formattingTuple.getMessage();
+        if (result == null)
+            throw new IllegalArgumentException("Given string cannot be formatter using given object");
+
+        return result;
     }
 
 }
