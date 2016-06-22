307d501c9e621f01a3e260a2a45eb613493d62c8
Goodlike <henrisssss@hotmail.com>
2016-06-22 20:11:04 +0300

    Implemented synchronized FileAppender
    For times when configuring a logger is just too much of a pain in the ass

---
+++ src/main/java/eu/goodlike/io/FileAppender.java
@@ -0,0 +1,65 @@
+package eu.goodlike.io;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
+/**
+ * <pre>
+ * Synchronized appended for files
+ *
+ * In most cases a properly configured logger is preferred, but for simple file operations, this should suffice
+ * </pre>
+ */
+public final class FileAppender implements AutoCloseable {
+
+    /**
+     * Appends given string to the file of this appender
+     */
+    public synchronized void append(String string) throws IOException {
+        bufferedWriter.append(string);
+    }
+
+    /**
+     * Appends given string and a new line symbol to the file of this appender
+     */
+    public void appendLine(String string) throws IOException {
+        append(string + System.lineSeparator());
+    }
+
+    @Override
+    public void close() throws Exception {
+        bufferedWriter.close();
+    }
+
+    // CONSTRUCTORS
+
+    public Optional<FileAppender> ofFile(String filename) {
+        Path path = Paths.get(filename);
+        if (!Files.isWritable(path))
+            return Optional.empty();
+
+        BufferedWriter bufferedWriter;
+        try {
+            bufferedWriter = Files.newBufferedWriter(path, UTF_8, CREATE_NEW);
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+        return Optional.of(new FileAppender(bufferedWriter));
+    }
+
+    private FileAppender(BufferedWriter bufferedWriter) {
+        this.bufferedWriter = bufferedWriter;
+    }
+
+    // PRIVATE
+
+    private final BufferedWriter bufferedWriter;
+
+}
