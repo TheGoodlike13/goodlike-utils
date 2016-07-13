229d61189674ae1eccdd6463900baae28771fd2d
Goodlike <henrisssss@hotmail.com>
2016-07-13 12:46:15 +0300

    Updated documentation

--- src/main/java/eu/goodlike/test/TestableRunnable.java
+++ src/main/java/eu/goodlike/test/TestableRunnable.java
@@ -2,19 +2,33 @@ package eu.goodlike.test;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * <pre>
+ * Runnable which counts how many times it has been run
+ *
+ * You should verify that all threads that need to access this runnable have finished before asserting using its methods,
+ * otherwise the results will not be consistent
+ * </pre>
+ */
 public final class TestableRunnable implements Runnable {
 
-    @Override
-    public void run() {
-        timesRun.incrementAndGet();
-    }
-
+    /**
+     * @return true if this runnable has been run at least once, false otherwise
+     */
     public boolean hasBeenRun() {
         return totalTimesRun() > 0;
     }
 
+    /**
+     * @return amount of times this runnable has been run
+     */
     public int totalTimesRun() {
         return timesRun.get();
+    }
+
+    @Override
+    public void run() {
+        timesRun.incrementAndGet();
     }
 
     // CONSTRUCTORS
